63625fe6d48a68d5375fa9ccbda860fff895fd9e
Samir Khadka <47472827+samirkhadka998@users.noreply.github.com>
2023-12-17 08:39:39 +0545

    additional test cases

--- sum.test.js
+++ sum.test.js
@@ -9,3 +9,21 @@ test('assign object', ()=>{
     data['color'] = 'Yellow';
     expect(data).toEqual({toy: 'Bear', color:'Yellow'})
 })
+
+test('adding positive number is not zero', ()=>{
+    for(let i = 1; i < 10; i++){
+        for(let j = 1 ; j < 10 ; j++){
+            expect(i + j).not.toBe(0);
+        }
+    }
+})
+
+
+
+test('two plus two', () => {
+    const value = 2+2;
+    expect(value).toBe(4);
+    expect(value).not.toBe(5);
+    expect(value).toEqual(4);
+    expect(value).toBeGreaterThan(8);
+})
